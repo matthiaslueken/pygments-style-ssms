9d6d8dd9dc31636789c13c3ecab3e00f1393d3eb
Matthias Lüken <mlueken@heringer-consulting.de>
2021-01-21 12:38:00 +0100

    fixed

---
+++ pygments_style_ssms/ssms.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.style.ssms
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    A Pygments style inspired by Microsoft SSMS.
+
+    :copyright: Copyright 2021 Matthias Lüken
+    :license: BSD, see LICENSE for details.
+"""
+
+from pygments.style import Style
+from pygments.token import Keyword, Name, Comment, String, Error, \
+     Number, Operator
+
+
+class SSMSStyle(Style):
+    """
+    A Pygments style inspired by Microsoft SSMS.
+    """
+
+    background_color = "#ffffff"
+    default_style = ""
+
+    styles = {
+        Comment:                   "#008000",
+        Keyword:                   "#0000ff",
+        Operator:                  "#808080",
+        Name:                      "#000000",
+        Name.Function:             "#ff00ff",
+        Name.Builtin:              "#01ff00",
+        String:                    "#ff0000",
+        Error:                     "border:#ff0000",
+        Number:                    "#000000"
+    }
---
+++ setup.py
@@ -0,0 +1,34 @@
+﻿# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pygments-style-ssms',
+    version='0.5',
+    description='A Pygments style inspired by Microsoft SSMS.',
+    keywords='pygments style ssms',
+    license='BSD',
+
+    author='Matthias Lüken',
+
+    url='https://github.com/matthiaslueken/pygments-style-ssms',
+
+    packages=find_packages(),
+    install_requires=['pygments >= 2.2'],
+
+    entry_points='''[pygments.styles]
+                    ssms=pygments_style_ssms:SSMSStyle''',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
